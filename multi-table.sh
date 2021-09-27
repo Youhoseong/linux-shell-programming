db5847af733f1c4c3a03e1f4ecda5e4e4ccfcf84
Youhoseong <h0song@naver.com>
2021-09-27 15:39:01 +0900

    feat: input validate add

--- multi-table.sh
+++ multi-table.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # 20172256 Youhoseong Linux Assignment 3
+
+
+if [ $# -ne 2 ]; then
+	echo "Argument Count must be 2."
+	exit 1
+fi
+
+
 row=$1
 col=$2
+
 
 if [ $row -lt 1 ] || [ $col -lt 1 ]; then
 	echo "Argument must be greater than 0."
